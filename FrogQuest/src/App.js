56913d32626c8dd1c22c526ab8040bb89c48ddd8
Fabio <alvesfabio2004@gmail.com>
2021-04-06 16:21:48 -0300

    Primeiros scripts

---
+++ FrogQuest/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Botoes from './components/botoes.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Botoes nome='/play' />
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ FrogQuest/src/components/botoes.js
@@ -0,0 +1,10 @@
+import React from "react";
+import btnPlay from "../res/play.png"
+
+export default function(props){
+    
+    return(
+        <img src={btnPlay} />
+        //<img src={path + props.nome} />
+    )
+}
