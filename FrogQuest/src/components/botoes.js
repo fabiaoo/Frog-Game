ba97884e398a91d40933849001173f19efff4f8f
keikoligia <70450947+keikoligia@users.noreply.github.com>
2021-04-06 21:23:59 -0300

    Início do Menu
    Implementação do logo e botões (não funcionais ainda).

--- FrogQuest/src/components/botoes.js
+++ FrogQuest/src/components/botoes.js
@@ -1,10 +1,18 @@
 import React from "react";
+import './botoesStyle.css';
 import btnPlay from "../res/play.png"
+import logo from '../res/logo.png'
 
 export default function(props){
     
     return(
-        <img src={btnPlay} />
-        //<img src={path + props.nome} />
+        <div className="menu">
+            <img src={logo} alt="logo do sapinho" id="logo"/>
+            <div>
+            <input type="image" className="btn" name="play" src={btnPlay} />        
+            <input type="image" className="btn" name="play" src={btnPlay} />        
+            </div>
+        </div>
+        //<img src={path + props.nome} />            //<img src={btnPlay} />
     )
 }
